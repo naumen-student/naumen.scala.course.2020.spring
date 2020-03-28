242e2218a4f8623f330230b5a2385ebec094bd0f
Данил <sdl@renue.com>
2020-03-28 20:52:05 +0500

    Выполнена домашняя работа номер 2

--- homeworks/homework_2/src/main/scala/Exercises.scala
+++ homeworks/homework_2/src/main/scala/Exercises.scala
@@ -1,3 +1,5 @@
+import scala.annotation.tailrec
+
 object Exercises {
 
     /*ПРИМЕР*/
@@ -16,8 +18,9 @@ object Exercises {
     /*Реализовать функцию, которая возвращает сумму всех целых чисел в заданном диапазоне (от iForm до iTo), которые делятся
     на 3 или на 5.*/
     /*Реализовать юнит-тесты в src/test/scala для данной функции.*/
-    def sumOfDivBy3Or5(iFrom: Int, iTo: Int): Long = ???
-
+    def sumOfDivBy3Or5(iFrom: Int, iTo: Int): Long = {
+        iFrom to iTo filter( num => num % 3 == 0|| num % 5 == 0) sum
+    }
 
 
     /*ЗАДАНИЕ II*/
@@ -25,7 +28,15 @@ object Exercises {
     Число 80 раскладывается на множители 1 * 2 * 2 * 2 * 2 * 5, результат выполнения функции => Seq(2, 5).
     Число 98 можно разложить на множители 1 * 2 * 7 * 7, результат выполнения функции => Seq(2, 7).*/
     /*Реализовать юнит-тесты в src/test/scala для данной функции.*/
-    def primeFactor(number: Int): Seq[Int] = ???
+    def primeFactor(number: Int): Seq[Int] = {
+        @tailrec
+        def addPrimeDelimiter(number: Int, delimiter: Int = 2, list: List[Int] = Nil): List[Int] = delimiter*delimiter > number match {
+            case false if number % delimiter == 0 => addPrimeDelimiter(number / delimiter, delimiter, delimiter :: list)
+            case false               => addPrimeDelimiter(number    , delimiter + 1, list)
+            case true                => number :: list
+        }
+        addPrimeDelimiter(number).toSet.toSeq.sorted
+    }
 
 
 
@@ -40,15 +51,15 @@ object Exercises {
     def abs(vec: Vector2D): Double = java.lang.Math.sqrt(vec.x * vec.x + vec.y * vec.y)
     def scalar(vec0: Vector2D, vec1: Vector2D): Double = vec0.x * vec1.x + vec0.y * vec1.y
     def cosBetween(vec0: Vector2D, vec1: Vector2D): Double = scalar(vec0, vec1) / abs(vec0) / abs(vec1)
-    //def sumByFunc(leftVec0: Vector2D, leftVec1: Vector2D, ???, rightVec0: Vector2D, rightVec1: Vector2D) = ???
-    /*
+
+    def sumByFunc(leftVec0: Vector2D, leftVec1: Vector2D, func: (Vector2D, Vector2D) => Double,
+                  rightVec0: Vector2D, rightVec1: Vector2D): Double = {
+        func(leftVec0, leftVec1) + func(rightVec0, rightVec1)
+    }
     def sumScalars(leftVec0: Vector2D, leftVec1: Vector2D, rightVec0: Vector2D, rightVec1: Vector2D): Double =
         sumByFunc(leftVec0, leftVec1, scalar, rightVec0, rightVec1)
-    */
-    /*
     def sumCosines(leftVec0: Vector2D, leftVec1: Vector2D, rightVec0: Vector2D, rightVec1: Vector2D): Double =
         sumByFunc(leftVec0, leftVec1, cosBetween, rightVec0, rightVec1)
-    */
 
 
 
@@ -71,6 +82,15 @@ object Exercises {
             "Chrome" ->   (3,   7.18),   "Cesium" ->    (7,   1.873), "Zirconium" -> (3,   6.45)
         )
 
-    def sortByHeavyweight(ballsArray: Map[String, (Int, Double)] = balls): Seq[String] = ???
+    def sortByHeavyweight(ballsArray: Map[String, (Int, Double)] = balls): Seq[String] = {
+        val volumeCoefficient = Math.PI * 4 / 3
+
+        def getBallMass(radius: Int, density: Double): Double = {
+            volumeCoefficient * math.pow(radius, 3) * density
+        }
+        ballsArray.map{case (name, (radius, density)) =>
+            name -> getBallMass(radius, density)
+        }.toSeq.sortBy(nameAndMass => nameAndMass._2).map(nameAndMass => nameAndMass._1)
+    }
 
 }
--- homeworks/homework_2/src/test/scala/Test.scala
+++ homeworks/homework_2/src/test/scala/Test.scala
@@ -3,10 +3,44 @@ import utest._
 object Test extends TestSuite{
 
     val tests = Tests{
+        val a = Exercises.primeFactor(80)
+
         'test_divBy3Or7 - {
             assert(Exercises.divBy3Or7(1, 3) == Seq(3))
             assert(Exercises.divBy3Or7(5, 9) == Seq(6, 7, 9))
             assert(Exercises.divBy3Or7(0, 100) == Seq(0, 3, 6, 7, 9, 12, 14, 15, 18, 21, 24, 27, 28, 30, 33, 35, 36, 39, 42, 45, 48, 49, 51, 54, 56, 57, 60, 63, 66, 69, 70, 72, 75, 77, 78, 81, 84, 87, 90, 91, 93, 96, 98, 99))
+        }
+
+        'sumOfDivBy3Or5 - {
+            assert(Exercises.sumOfDivBy3Or5(1, 3) == 3)
+            assert(Exercises.sumOfDivBy3Or5(5, 10) == 30)
+        }
+
+        'primeFactor - {
+            assert(Exercises.primeFactor(80) == Seq(2, 5))
+            assert(Exercises.primeFactor(98) == Seq(2, 7))
+        }
+
+        'sumScalars -{
+            assert(Exercises.sumScalars(Exercises.Vector2D(1, 2), Exercises.Vector2D(-3, 4), Exercises.Vector2D(5, 6), Exercises
+              .Vector2D(-8, 9)) == 19.0)
+            assert(Exercises.sumScalars(Exercises.Vector2D(-1, -2), Exercises.Vector2D(3, 4), Exercises.Vector2D(-5, -6),
+                Exercises
+              .Vector2D(8, 9)) == -105.0)
+        }
+
+        'sumCosines -{
+            assert(Exercises.sumCosines(Exercises.Vector2D(1, 1), Exercises.Vector2D(2, 2), Exercises.Vector2D(3, 3), Exercises
+              .Vector2D(4, 4)) == 2.0)
+            assert(Exercises.sumCosines(Exercises.Vector2D(1, 1), Exercises.Vector2D(-2, 2), Exercises.Vector2D(3, 3), Exercises
+              .Vector2D(4, 4)) == 1.0)
+        }
+
+        'sortByHeavyweight - {
+            val sortedBalls = Exercises.sortByHeavyweight()
+            assert(sortedBalls.head == "Tin")
+            assert(sortedBalls.last == "Graphite")
+            assert(sortedBalls.indexOf("Gold") < sortedBalls.indexOf("Tungsten"))
         }
     }
 }
