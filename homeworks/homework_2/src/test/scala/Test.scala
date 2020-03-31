5ff24fa17b1f6864e99b223d69203e54048f8eee
Imessal <38953768+Imessal@users.noreply.github.com>
2020-03-31 14:34:04 +0500

    solved

--- homeworks/homework_2/src/test/scala/Test.scala
+++ homeworks/homework_2/src/test/scala/Test.scala
@@ -1,12 +1,38 @@
 import utest._
+import Exercises.Vector2D
 
-object Test extends TestSuite{
+object Test extends TestSuite {
 
-    val tests = Tests{
+    val tests = Tests {
         'test_divBy3Or7 - {
             assert(Exercises.divBy3Or7(1, 3) == Seq(3))
             assert(Exercises.divBy3Or7(5, 9) == Seq(6, 7, 9))
             assert(Exercises.divBy3Or7(0, 100) == Seq(0, 3, 6, 7, 9, 12, 14, 15, 18, 21, 24, 27, 28, 30, 33, 35, 36, 39, 42, 45, 48, 49, 51, 54, 56, 57, 60, 63, 66, 69, 70, 72, 75, 77, 78, 81, 84, 87, 90, 91, 93, 96, 98, 99))
+        }
+
+        'test_sumOfDivBy3Or5 - {
+            assert(Exercises.sumOfDivBy3Or5(1, 10) == 33)
+            assert(Exercises.sumOfDivBy3Or5(10, 20) == 75)
+            assert(Exercises.sumOfDivBy3Or5(21, 33) == 160)
+            assert(Exercises.sumOfDivBy3Or5(1, 2) == 0)
+        }
+
+        'test_primeFactor - {
+            assert(Exercises.primeFactor(10) == Seq(2, 5))
+            assert(Exercises.primeFactor(98) == Seq(2, 7))
+            assert(Exercises.primeFactor(80) == Seq(2, 5))
+            assert(Exercises.primeFactor(360) == Seq(2, 3, 5))
+        }
+
+        'test_sumByFunc - {
+            assert(Exercises.sumScalars(Vector2D(1, 2), Vector2D(-3, 4), Vector2D(5, 2), Vector2D(3, -3)) == 14)
+            assert(Exercises.sumCosines(Vector2D(1, 2), Vector2D(-3, 4), Vector2D(5, 2), Vector2D(3, -3)) <= 0.88)
+        }
+
+        'test_sortByHeavyweight - {
+            assert(Exercises.sortByHeavyweight(Map("" -> (0, 0))) == Seq(""))
+            assert(Exercises.sortByHeavyweight(Map("a" -> (1, 1), "b" -> (2, 2), "c" -> (3, 3))) == Seq("a", "b", "c"))
+            assert(Exercises.sortByHeavyweight().take(5) == Seq("Tin", "Platinum", "Nickel", "Aluminum", "Titanium"))
         }
     }
 }
