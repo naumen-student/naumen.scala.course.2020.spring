3adefbefcc08e8476bbdd465de791e77a5434662
BMW835 <dprq@bk.ru>
2020-04-01 02:29:08 +0500

    homework_2

--- homeworks/homework_2/src/test/scala/Test.scala
+++ homeworks/homework_2/src/test/scala/Test.scala
@@ -8,5 +8,33 @@ object Test extends TestSuite{
             assert(Exercises.divBy3Or7(5, 9) == Seq(6, 7, 9))
             assert(Exercises.divBy3Or7(0, 100) == Seq(0, 3, 6, 7, 9, 12, 14, 15, 18, 21, 24, 27, 28, 30, 33, 35, 36, 39, 42, 45, 48, 49, 51, 54, 56, 57, 60, 63, 66, 69, 70, 72, 75, 77, 78, 81, 84, 87, 90, 91, 93, 96, 98, 99))
         }
+
+        'test_sumOfDivBy3Or5 - {
+            assert(Exercises.sumOfDivBy3Or5(1, 3) == 3)
+            assert(Exercises.sumOfDivBy3Or5(5, 9) == 20)
+            assert(Exercises.sumOfDivBy3Or5(0, 100) == 2418)
+        }
+
+        'test_primeFactor - {
+            assert(Exercises.primeFactor(13) == Seq(13))
+            assert(Exercises.primeFactor(98) == Seq(2, 7))
+            assert(Exercises.primeFactor(240) == Seq(2, 3, 5))
+        }
+
+        'test_Vector2D - {
+            assert(Exercises.sumScalars(Exercises.Vector2D(1, 1),
+                                        Exercises.Vector2D(2, 2),
+                                        Exercises.Vector2D(3, 3),
+                                        Exercises.Vector2D(4, 4)) == 28)
+            assert(Exercises.sumScalars(Exercises.Vector2D(1, 1),
+                                        Exercises.Vector2D(2, 2),
+                                        Exercises.Vector2D(3, 3),
+                                        Exercises.Vector2D(4, 4)) == 28)
+
+        }
+
+        'test_sortByHeavyweight - {
+            assert(Exercises.sortByHeavyweight() == Seq("Tin", "Platinum", "Nickel", "Aluminum", "Titanium", "Lead", "Sodium", "Uranium", "Gold", "Tungsten", "Zirconium", "Chrome", "Iron", "Copper", "Silver", "Plutonium", "Cobalt", "Cesium", "Calcium", "Lithium", "Magnesium", "Potassium", "Graphite"))
+        }
     }
 }
