3e8e9ca3dbaaec81b57c97971e635ae643e10b9b
Timofey Utrobin <utrobin.timofey@yandex.ru>
2020-04-02 14:35:40 +0500

    homework_2 - Утробин Тимофей

--- homeworks/homework_2/src/main/scala/Exercises.scala
+++ homeworks/homework_2/src/main/scala/Exercises.scala
@@ -16,7 +16,13 @@ object Exercises {
     /*Реализовать функцию, которая возвращает сумму всех целых чисел в заданном диапазоне (от iForm до iTo), которые делятся
     на 3 или на 5.*/
     /*Реализовать юнит-тесты в src/test/scala для данной функции.*/
-    def sumOfDivBy3Or5(iFrom: Int, iTo: Int): Long = ???
+    def sumOfDivBy3Or5(iFrom: Int, iTo: Int): Long = {
+        if (iFrom > iTo)
+            throw new IllegalArgumentException("iFrom must be greater or equal than iTo")
+        (iFrom to iTo)
+            .filter(number => number % 3 == 0 || number % 5 == 0)
+            .sum
+    }
 
 
 
@@ -25,8 +31,24 @@ object Exercises {
     Число 80 раскладывается на множители 1 * 2 * 2 * 2 * 2 * 5, результат выполнения функции => Seq(2, 5).
     Число 98 можно разложить на множители 1 * 2 * 7 * 7, результат выполнения функции => Seq(2, 7).*/
     /*Реализовать юнит-тесты в src/test/scala для данной функции.*/
-    def primeFactor(number: Int): Seq[Int] = ???
+    def primeFactor(number: Int): Seq[Int] = {
+        var factors = Set[Int]()
 
+        var dividend = number
+        var divisor = 2
+        while (divisor * divisor <= dividend) {
+            if (dividend % divisor == 0) {
+                factors += divisor
+                dividend /= divisor
+            } else {
+                divisor += 1
+            }
+        }
+
+        if (dividend > 1) factors += dividend
+
+        factors.toSeq
+    }
 
 
     /*ЗАДАНИЕ III*/
@@ -40,17 +62,21 @@ object Exercises {
     def abs(vec: Vector2D): Double = java.lang.Math.sqrt(vec.x * vec.x + vec.y * vec.y)
     def scalar(vec0: Vector2D, vec1: Vector2D): Double = vec0.x * vec1.x + vec0.y * vec1.y
     def cosBetween(vec0: Vector2D, vec1: Vector2D): Double = scalar(vec0, vec1) / abs(vec0) / abs(vec1)
-    //def sumByFunc(leftVec0: Vector2D, leftVec1: Vector2D, ???, rightVec0: Vector2D, rightVec1: Vector2D) = ???
-    /*
+    def sumByFunc(
+                   leftVec0: Vector2D,
+                   leftVec1: Vector2D,
+                   func: (Vector2D, Vector2D) => Double,
+                   rightVec0: Vector2D,
+                   rightVec1: Vector2D
+                 ): Double = {
+        func(leftVec0, leftVec1) + func(rightVec0, rightVec1)
+    }
+
     def sumScalars(leftVec0: Vector2D, leftVec1: Vector2D, rightVec0: Vector2D, rightVec1: Vector2D): Double =
         sumByFunc(leftVec0, leftVec1, scalar, rightVec0, rightVec1)
-    */
-    /*
+
     def sumCosines(leftVec0: Vector2D, leftVec1: Vector2D, rightVec0: Vector2D, rightVec1: Vector2D): Double =
         sumByFunc(leftVec0, leftVec1, cosBetween, rightVec0, rightVec1)
-    */
-
-
 
     /*ЗАДАНИЕ IV*/
     /*Дано: коллекция металлических шариков balls, где каждый элемент представлен в виде (Name: String -> (radius: Int, density: Double).
@@ -71,6 +97,10 @@ object Exercises {
             "Chrome" ->   (3,   7.18),   "Cesium" ->    (7,   1.873), "Zirconium" -> (3,   6.45)
         )
 
-    def sortByHeavyweight(ballsArray: Map[String, (Int, Double)] = balls): Seq[String] = ???
-
+    def sortByHeavyweight(ballsArray: Map[String, (Int, Double)] = balls): Seq[String] = {
+        ballsArray
+            .toList
+            .sortBy(ball => 4 / 3 * ball._2._2 * java.lang.Math.PI * java.lang.Math.pow(ball._2._1, 3))
+            .map(_._1)
+    }
 }
--- homeworks/homework_2/src/test/scala/Test.scala
+++ homeworks/homework_2/src/test/scala/Test.scala
@@ -8,5 +8,143 @@ object Test extends TestSuite{
             assert(Exercises.divBy3Or7(5, 9) == Seq(6, 7, 9))
             assert(Exercises.divBy3Or7(0, 100) == Seq(0, 3, 6, 7, 9, 12, 14, 15, 18, 21, 24, 27, 28, 30, 33, 35, 36, 39, 42, 45, 48, 49, 51, 54, 56, 57, 60, 63, 66, 69, 70, 72, 75, 77, 78, 81, 84, 87, 90, 91, 93, 96, 98, 99))
         }
+
+        'test_sumOfDivBy3Or5 - {
+            'simpleRange - {
+                assert(Exercises.sumOfDivBy3Or5(1, 5) == 8)
+            }
+            'fromNegativeToPositive - {
+                assert(Exercises.sumOfDivBy3Or5(-100, 100) == 0)
+                assert(Exercises.sumOfDivBy3Or5(-38, 55) == 424)
+            }
+            'sameNumber - {
+                assert(Exercises.sumOfDivBy3Or5(3, 3) == 3)
+                assert(Exercises.sumOfDivBy3Or5(-3, -3) == -3)
+            }
+            'fromLargeToSmallThrowsIllegalArgumentException - {
+                assert(
+                    try {
+                        Exercises.sumOfDivBy3Or5(5, 3)
+                        false
+                    } catch  {
+                        case _: IllegalArgumentException => true
+                    }
+                )
+            }
+        }
+
+        'test_primeFactor - {
+            'simpleNumber - {
+                assert(Exercises.primeFactor(2) == Seq(2))
+                assert(Exercises.primeFactor(80) == Seq(2, 5))
+                assert(Exercises.primeFactor(98) == Seq(2, 7))
+            }
+            'largeNumber - {
+                assert(Exercises.primeFactor(12345678) == Seq(2, 3, 47, 14593))
+            }
+            'negativeNumberMustReturnEmptySeq - {
+                assert(Exercises.primeFactor(-1337) == Seq())
+            }
+            'zeroMustReturnEmptySeq - {
+                assert(Exercises.primeFactor(0) == Seq())
+            }
+            'oneMustReturnEmptySeq - {
+                assert(Exercises.primeFactor(1) == Seq())
+            }
+        }
+
+        'test_sumScalars - {
+            'zeroVectors - {
+                assert(Exercises.sumScalars(
+                    Exercises.Vector2D(0, 0),
+                    Exercises.Vector2D(0, 0),
+                    Exercises.Vector2D(0, 0),
+                    Exercises.Vector2D(0, 0)
+                ) == 0)
+            }
+            'perpendicularVectors - {
+                assert(Exercises.sumScalars(
+                    Exercises.Vector2D(0, 1),
+                    Exercises.Vector2D(2, 0),
+                    Exercises.Vector2D(3.63, 0),
+                    Exercises.Vector2D(0, 36.5)
+                ) == 0)
+            }
+            'simpleVectors - {
+                assert({
+                    val sum = Exercises.sumScalars(
+                        Exercises.Vector2D(13.3, 4.4),
+                        Exercises.Vector2D(-12.5, 15.3),
+                        Exercises.Vector2D(100.253, 0.32),
+                        Exercises.Vector2D(20, -20)
+                    )
+                    math.abs(sum - 1899.73) <= 0.000000000001
+                })
+            }
+        }
+
+        'test_sumCosines - {
+            'zeroVectorsReturnNaN - {
+                assert(
+                    Exercises.sumCosines(
+                        Exercises.Vector2D(0, 0),
+                        Exercises.Vector2D(0, 0),
+                        Exercises.Vector2D(0, 0),
+                        Exercises.Vector2D(0, 0)
+                    ).isNaN
+                )
+            }
+            'perpendicularVectors - {
+                assert(Exercises.sumCosines(
+                    Exercises.Vector2D(0, 1),
+                    Exercises.Vector2D(2, 0),
+                    Exercises.Vector2D(3.63, 0),
+                    Exercises.Vector2D(0, 36.5)
+                ) == 0)
+            }
+            'simpleVectors - {
+                assert({
+                    val sum = Exercises.sumCosines(
+                        Exercises.Vector2D(13.3, 4.4),
+                        Exercises.Vector2D(-12.5, 15.3),
+                        Exercises.Vector2D(100.253, 0.32),
+                        Exercises.Vector2D(20, -20)
+                    )
+                    math.abs(sum - (0.704846159164554 - 0.35743876218872633)) <= 0.000000000001
+                })
+            }
+        }
+
+        'test_sortByHeavyweight - {
+            'correctOrder - {
+                assert(
+                    Exercises.sortByHeavyweight() == Seq(
+                        "Tin",
+                        "Platinum",
+                        "Nickel",
+                        "Aluminum",
+                        "Titanium",
+                        "Lead",
+                        "Sodium",
+                        "Uranium",
+                        "Gold",
+                        "Tungsten",
+                        "Zirconium",
+                        "Chrome",
+                        "Iron",
+                        "Copper",
+                        "Silver",
+                        "Plutonium",
+                        "Cobalt",
+                        "Cesium",
+                        "Calcium",
+                        "Lithium",
+                        "Magnesium",
+                        "Potassium",
+                        "Graphite"
+                    )
+                )
+            }
+        }
     }
 }
