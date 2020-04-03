8de70a8a873c457a1542d01de63093f217a79f58
TVIGes <khdv66@gmail.com>
2020-04-03 20:29:45 +0500

    solve tasks

--- homeworks/homework_3/src/test/scala/homework_3/Test.scala
+++ homeworks/homework_3/src/test/scala/homework_3/Test.scala
@@ -5,9 +5,59 @@ import utest._
 object Test extends TestSuite{
 
     val tests = Tests{
-        'test_example - {
-            val trueStr = "правда"
-            assert(Exercises.prettyBooleanFormatter1(true) == trueStr)
+        'test_prettyBooleanFormatter1 - {
+            testBooleanFormatter(Exercises.prettyBooleanFormatter1)
+        }
+        'test_prettyBooleanFormatter2 - {
+            testBooleanFormatter(Exercises.prettyBooleanFormatter1)
+        }
+        'test_prettyBooleanFormatter3 - {
+            testBooleanFormatter(Exercises.prettyBooleanFormatter1)
+        }
+
+        def testBooleanFormatter(format: Any => String): Unit = {
+            assert(format(true) == "правда")
+            assert(format(false) == "ложь")
+            assert(format(1) == "1")
+            assert(format(()) == "()")
+            assert(format("истина") == "истина")
+        }
+
+        'test_max1 - {
+            intercept[UnsupportedOperationException](Exercises.max1(Seq.empty))
+            assert(Exercises.max1(Seq(1)) == 1)
+            assert(Exercises.max1(Seq(1, 2, 3)) == 3)
+            assert(Exercises.max1(Seq(3, 2, 1)) == 3)
+        }
+        'test_max2 - {
+            assert(Exercises.max2(Seq.empty).isEmpty)
+            assert(Exercises.max2(Seq(1)) == Seq(1))
+            assert(Exercises.max2(Seq(1, 2, 3)) == Seq(3))
+            assert(Exercises.max2(Seq(3, 2, 1)) == Seq(3))
+        }
+        'test_max3 - {
+            assert(Exercises.max3(Seq.empty).isEmpty)
+            assert(Exercises.max3(Seq(1)).get == 1)
+            assert(Exercises.max3(Seq(1, 2, 3)).get == 3)
+            assert(Exercises.max3(Seq(3, 2, 1)).get == 3)
+        }
+
+        'test_sum1 - {
+            testSum(Exercises.sum1)
+        }
+        'test_sum2 - {
+            testSum(Exercises.sum2)
+        }
+        'test_sum3 - {
+            testSum(Exercises.sum3)
+        }
+
+        def testSum(sum: (Int, Int) => Int): Unit = {
+            assert(sum(0, 0) == 0)
+            assert(sum(1, 2) == 3)
+            assert(sum(-1, 2) == 1)
+            assert(sum(1, -2) == -1)
+            assert(sum(-1, -2) == -3)
         }
     }
 }
