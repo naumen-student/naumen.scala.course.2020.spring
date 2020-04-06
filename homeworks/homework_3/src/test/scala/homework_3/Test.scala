6bebe2aea553c5ec822e13cd85f3ac532a880047
eliyy4hu <eliyy4hu@outlook.com>
2020-04-06 17:50:31 +0500

    done hw_3

--- homeworks/homework_3/src/test/scala/homework_3/Test.scala
+++ homeworks/homework_3/src/test/scala/homework_3/Test.scala
@@ -2,12 +2,40 @@ package homework_3
 
 import utest._
 
-object Test extends TestSuite{
+object Test extends TestSuite {
 
-    val tests = Tests{
-        'test_example - {
-            val trueStr = "правда"
-            assert(Exercises.prettyBooleanFormatter1(true) == trueStr)
-        }
+  val tests = Tests {
+    'test_prettyBooleanFormatter1 - {
+      val trueStr = "правда"
+      assert(Exercises.prettyBooleanFormatter1(true) == trueStr)
     }
+    'test_prettyBooleanFormatter2 - {
+      val trueStr = "правда"
+      assert(Exercises.prettyBooleanFormatter2(true) == trueStr)
+    }
+    'test_prettyBooleanFormatter3 - {
+      val trueStr = "правда"
+      assert(Exercises.prettyBooleanFormatter3(true) == trueStr)
+    }
+    'test_max1 - {
+      assert(Exercises.max1(Seq(1, 2, 3)) == 3)
+    }
+    'test_max2 - {
+      assert(Exercises.max2(Seq(3, 1, 2, 3)) == Seq(3, 3))
+      assert(Exercises.max2(Seq()) == Seq())
+    }
+    'test_max3 - {
+      assert(Exercises.max3(Seq(1, 2, 3)) == Option(3))
+      assert(Exercises.max3(Seq()) == None)
+    }
+    'test_sum1 - {
+      assert(Exercises.sum1(1, 2) == 3)
+    }
+    'test_sum2 - {
+      assert(Exercises.sum2(1, 2) == 3)
+    }
+    'test_sum3 - {
+      assert(Exercises.sum3(1, 2) == 3)
+    }
+  }
 }
