dba0ed34b008257a65ace3869ebbe30b4f6cb931
Resinchen <alex1410alex@gmail.com>
2020-04-07 18:06:49 +0500

    homework 3

--- homeworks/homework_3/src/test/scala/homework_3/Test.scala
+++ homeworks/homework_3/src/test/scala/homework_3/Test.scala
@@ -5,9 +5,60 @@ import utest._
 object Test extends TestSuite{
 
     val tests = Tests{
-        'test_example - {
-            val trueStr = "правда"
-            assert(Exercises.prettyBooleanFormatter1(true) == trueStr)
+        val trueStr = "правда"
+        val falseStr = "ложь"
+        val someInt = "42"
+
+        'test_prettyBooleanFormatter1 - {
+            Exercises.prettyBooleanFormatter1(true) == trueStr
+            Exercises.prettyBooleanFormatter1(false) == falseStr
+            Exercises.prettyBooleanFormatter1(42) == someInt
+        }
+        'test_prettyBooleanFormatter2 - {
+            Exercises.prettyBooleanFormatter2(true) == trueStr
+            Exercises.prettyBooleanFormatter2(false) == falseStr
+            Exercises.prettyBooleanFormatter2(42) == someInt
+        }
+        'test_prettyBooleanFormatter3 - {
+            Exercises.prettyBooleanFormatter3(true) == trueStr
+            Exercises.prettyBooleanFormatter3(false) == falseStr
+            Exercises.prettyBooleanFormatter3(42) == someInt
+        }
+
+        val simpleSeq: Seq[Int] = Seq(4, 5, 1, 2, 6, 4, 1, 3)
+        val emptySeq: Seq[Int] = Seq()
+        val multipleSeq: Seq[Int] = Seq(5, 3, 6, 1, 8, 4, 8, 1)
+
+        'test_max1 - {
+            Exercises.max1(simpleSeq) ==> 6
+            Exercises.max1(emptySeq) ==> -1
+            Exercises.max1(multipleSeq) ==> 8
+        }
+        'test_max2 - {
+            Exercises.max2(simpleSeq) ==> Seq(6)
+            Exercises.max2(emptySeq) ==> Seq()
+            Exercises.max2(multipleSeq) ==> Seq(8, 8)
+        }
+        'test_max3 - {
+            Exercises.max3(simpleSeq) ==> Some(6)
+            Exercises.max3(emptySeq) ==> None
+            Exercises.max3(multipleSeq) ==> Some(8)
+        }
+
+        'test_sum1 - {
+            Exercises.sum1(1, -1) == 0
+            Exercises.sum1(15, 25) == 40
+            Exercises.sum1(0, 0) == 0
+        }
+        'test_sum2 - {
+            Exercises.sum2(1, -1) == 0
+            Exercises.sum2(15, 25) == 40
+            Exercises.sum2(0, 0) == 0
+        }
+        'test_sum3 - {
+            Exercises.sum3(1, -1) == 0
+            Exercises.sum3(15, 25) == 40
+            Exercises.sum3(0, 0) == 0
         }
     }
 }
