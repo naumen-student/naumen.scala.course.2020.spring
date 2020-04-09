034e05223c28a750a812ee73a40978aae86100e6
A.Voi <voytsishevskiy@tochka.com>
2020-04-09 14:53:12 +0500

    solve recursive part;

--- homeworks/homework_4/src/main/scala/Cell.scala
+++ homeworks/homework_4/src/main/scala/Cell.scala
@@ -1,41 +1,34 @@
 import scala.annotation.tailrec
-import scala.collection.mutable
 
 sealed trait Cell extends Product with Serializable {
   override def toString: String = this match {
-    case EmptyCell()                  => "empty"
-    case NumberCell(number)             => number.toString
-    case StringCell(text)           => text
-    case r @ ReferenceCell(_, _, _) => r.toString
+    case EmptyCell()        => "empty"
+    case NumberCell(number) => number.toString
+    case StringCell(text)   => text
+    case r: ReferenceCell   => r.toString
   }
 
 }
 
 case class /*object*/ EmptyCell() extends Cell
 
-final case class NumberCell(number: Int)  extends Cell
+final case class NumberCell(number: Int) extends Cell
 
 final case class StringCell(text: String) extends Cell
 
 final case class ReferenceCell(x: Int, y: Int, table: Table) extends Cell {
-  override def toString: String = {
-    val set = mutable.Set.empty[ReferenceCell]
-    @tailrec
-    def doesHaveCycle(visited: mutable.Set[ReferenceCell], c: Cell) : Boolean = c match {
-      case r @ ReferenceCell(x, y, table) =>
-        if (visited.contains(r)) {
-          false
-        } else {
-          table getCell(x, y) match {
-            case Some(cell) => doesHaveCycle(visited + r, cell)
-            case None       => false
+  @tailrec
+  private def cellToString(cell: Cell, visited: Set[ReferenceCell]): String =
+    cell match {
+      case ref @ ReferenceCell(x, y, table) =>
+        if (visited.contains(ref)) "cyclic"
+        else
+          table.getCell(x, y) match {
+            case Some(value) => cellToString(value, visited + ref)
+            case None        => "outOfRange"
           }
-        }
-      case _ => false
+      case cell => cell.toString
     }
-    if (doesHaveCycle(set,this)) "cyclic" else table.getCell(x, y) match {
-      case Some(value) => value.toString
-      case None =>"outOfRange"
-    }
-  }
+
+  override def toString: String = table.getCell(x, y).fold("outOfRange")(cellToString(_, Set(this)))
 }
