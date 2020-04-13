57932fe01871d47eee311be98ad8f7d5232b6986
ekguseva <crochik@mail.ru>
2020-04-14 00:02:53 +0500

    review class Table

--- homeworks/homework_4/src/main/scala/Table.scala
+++ homeworks/homework_4/src/main/scala/Table.scala
@@ -9,23 +9,14 @@ case class Table(val width:Int, val length:Int){
   }
 
   def getCell(ix:Int,iy:Int):Option[Cell] ={
-    if(inTable(ix,iy)){
-      Some(r((ix,iy)))
-    }
-    else None
+    if(inTable(ix,iy)) Some(r((ix,iy))) else None
   }
 
   def setCell(ix:Int,iy:Int,cell:Cell): Unit = {
-    if(inTable(ix,iy)){
-      r += ((ix,iy) -> cell)
-    }
-    else None
+    if(inTable(ix,iy)) r += ((ix,iy) -> cell)  else None
   }
 
   def inTable(ix:Int,iy:Int):Boolean={
-    if(ix>width || ix<0 || iy>length || iy<0){
-      false
-    }
-    else true
+    if(ix>width || ix<0 || iy>length || iy<0) false else true
   }
 }
