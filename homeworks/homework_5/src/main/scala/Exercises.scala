6e0ba89ca633a5dc3b4be2187199b9fea7ccb260
Artemiy <38810090+OptimumDev@users.noreply.github.com>
2020-04-16 14:08:19 +0500

    tasks solved

--- homeworks/homework_5/src/main/scala/Exercises.scala
+++ homeworks/homework_5/src/main/scala/Exercises.scala
@@ -1,23 +1,27 @@
 object Exercises {
-  trait Animal {
-    def name: String
-  }
 
-  case class Cat(override val name: String) extends Animal
+    sealed trait Animal {
+        def name: String
+    }
 
-  case class Dog(override val name: String) extends Animal
+    case class Cat(override val name: String) extends Animal
 
+    case class Dog(override val name: String) extends Animal
 
+    case class Shelter[+A <: Animal](animals: List[A]) {
+        def +[B >: A <: Animal](otherAnimal: B): Shelter[B] = Shelter(animals :+ otherAnimal)
+        def ++[B >: A <: Animal](otherShelter: Shelter[B]): Shelter[B] = Shelter(animals ++ otherShelter.animals)
+        def getNames: Seq[String] = animals.map(_.name)
+        def feed[B >: A <: Animal](food: Food[B]): Seq[String] = animals.map(food.feed)
+    }
 
-  case class Shelter ...
+    sealed trait Food[A <: Animal] {
+        def feed(animal: A): String = s"${animal.name} eats ${this.toString.toLowerCase}"
+    }
 
+    case object Meat extends Food[Animal]
 
+    case object Milk extends Food[Cat]
 
-  trait Food ...
-
-  case object Meat extends Food[Animal] ...
-
-  case object Milk extends Food[Cat] ...
-
-  case object Bread extends Food[Dog] ...
+    case object Bread extends Food[Dog]
 }
