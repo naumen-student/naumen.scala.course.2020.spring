49f41aa40f571a9b2d96508b16346c07715d2313
a.savichev <a.savichev@skbkontur.ru>
2020-04-27 02:09:20 +0500

    homework_5

--- homeworks/homework_5/src/main/scala/Exercises.scala
+++ homeworks/homework_5/src/main/scala/Exercises.scala
@@ -8,16 +8,42 @@ object Exercises {
   case class Dog(override val name: String) extends Animal
 
 
+  case class Shelter[+A <: Animal](pets: List[A]) {
+    def +[P >: A <: Animal](pet: P): Shelter[P] = {
+      Shelter[P](pet :: pets)
+    }
 
-  case class Shelter ...
+    def ++[P >: A <: Animal](another: Shelter[P]): Shelter[P] = {
+      Shelter(pets ++ another.pets)
+    }
+
+    def getNames: Seq[String] = pets.map(e => e.name)
+
+    def feed(food: Food[A]): Seq[String] = {
+      pets.map(pet => food.feed(pet))
+    }
+  }
 
 
+  trait Food [-A <: Animal] {
+    def feed(animal: A): String
+  }
 
-  trait Food ...
+  case object Meat extends Food[Animal] {
+    override def toString: String = "meat"
 
-  case object Meat extends Food[Animal] ...
+    override def feed(animal: Animal): String = s"${animal.name} eats ${toString}"
+  }
 
-  case object Milk extends Food[Cat] ...
+  case object Milk extends Food[Cat] {
+    override def toString: String = "milk"
 
-  case object Bread extends Food[Dog] ...
+    override def feed(animal: Cat): String = s"${animal.name} eats ${toString}"
+  }
+
+  case object Bread extends Food[Dog] {
+    override def toString: String = "bread"
+
+    override def feed(animal: Dog): String = s"${animal.name} eats ${toString}"
+  }
 }
