a96114f27260ed1c667873608024347cd5d06a09
Polina Galeeva <polina.galeeva15@gmail.com>
2020-05-14 22:12:03 +0500

    homework_4

--- homeworks/homework_4/src/test/scala/Test.scala
+++ homeworks/homework_4/src/test/scala/Test.scala
@@ -1,11 +1,11 @@
 import utest._
 
 object Test extends TestSuite {
-    val tests = Tests {
+    val tests: Tests = Tests {
         'test_createTable - {
             val table = new Table(3, 3)
             for (i <- 0 until 9) {
-                assert(table.getCell(i / 3, i % 3).toString == Some("empty"))
+                assert(table.getCell(i / 3, i % 3).map(_.toString) == Some("empty"))
             }
             assert(table.getCell(0, -1).map(_.toString) == None)
             assert(table.getCell(-1, 0).map(_.toString) == None)
